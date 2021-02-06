c6aa18af052ea0d500a489d82ce6a42598f8917b
David Castello <davidcastellobatetgmail.com>
2021-02-06 15:11:35 +0100

    implement color palett gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_page_ui_flutter/screens/login_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xfffcd1d1),
+                  Color(0xffece2e1),
+                  Color(0xffd3e0dc),
+                  Color(0xffaee1e1),
+                ],
+                stops: [0.1, 0.4, 0.7, 0.9],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
